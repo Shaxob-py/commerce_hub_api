875039feed8c2289ccf84d7828f5ad9fc941297f
Shaxobff <xunuddinovshaxob@gmail.com>
2025-11-13 02:20:45 +0500

    update

--- database/product.py
+++ database/product.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Float, ForeignKey, String
+from enum import Enum
+
+from sqlalchemy import Float, ForeignKey, String, Numeric, Enum as SQLEnum
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -12,12 +14,18 @@ class Category(Model):
 
 
 class Product(Model):
+    class Valute(Enum):
+        SUM = 'SUM'
+        RUB = 'RUB'
+        USD = 'USD'
+
     name: Mapped[str]
-    price: Mapped[float]
+    price: Mapped[float] = mapped_column(Numeric(12, 0))
     description: Mapped[str]
     photo_url: Mapped[str]
     lat: Mapped[float] = mapped_column(Float)
     lng: Mapped[float] = mapped_column(Float)
+    currency: Mapped[Valute] = mapped_column(SQLEnum(Valute))
 
     user_id: Mapped[UUID] = mapped_column(
         UUID(as_uuid=True),
--- routers/product.py
+++ routers/product.py
@@ -1,1 +1,13 @@
+from fastapi import APIRouter, Depends
 
+from utils.jwt_token import get_current_user
+
+product = APIRouter()
+
+
+
+@product.post("/trips")
+async def create_tour(
+        data: TripSchema,
+        current_user=Depends(get_current_user),
+):
